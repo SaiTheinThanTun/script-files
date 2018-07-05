3e6e2f78492c630d280521c8c23c33f8279766d3
SaiTheinThanTunOnMac <theinthantun.sai@gmail.com>
2018-07-05 09:50:22 +0100

    day2

--- sp.R
+++ sp.R
@@ -12,36 +12,29 @@ setwd("~/OneDrive/Summer Project/data/")
 #saveRDS(umk,"umk.RDS")
 umk <- readRDS("umk.RDS")
 
+####CodeBook for main####
+if(FALSE){
+# #variable name
+# names.umk <- names(umk)
+# #variable label
+# lab.umk <- varlabel(umk)
+# #variable type 
+# type.umk <- sapply(umk, class)
+# #variable label values
+# labval.umk <- get.label.tables(umk)
+# labval.umk <- sapply(lapply(labval.umk,names),function(x) {paste(x,collapse = ", ")})
+# codebk <- cbind(names.umk, lab.umk, type.umk, labval.umk)
+# #head(codebk)
+# #dim(codebk)
+# #write.csv(codebk,"codebook_uMk_R.csv")
+}
+
 #subsetting only important variables
 selectedVar <- read.csv('selected_var.csv')
 
 ####lite version of main dataset####
 liteumk <- umk[,names(umk) %in% selectedVar$name]
 
-
-#head(umk)
-#str(umk)
-
-#variable name
-names.umk <- names(umk)
-
-#variable label
-lab.umk <- varlabel(umk)
-
-#variable type 
-type.umk <- sapply(umk, class)
-
-#variable label values
-labval.umk <- get.label.tables(umk)
-
-labval.umk <- sapply(lapply(labval.umk,names),function(x) {paste(x,collapse = ", ")})
-
-####CodeBook for main####
-codebk <- cbind(names.umk, lab.umk, type.umk, labval.umk)
-#head(codebk)
-#dim(codebk)
-
-#write.csv(codebk,"codebook_uMk_R.csv")
 
 ###exploratory####
 #InSilicoVA
@@ -56,8 +49,8 @@ for(i in 1:nrow(isv)){
 }
 CODdataset <- cbind(isvID,COD)
 
-
-# if(FALSE){
+####another way of getting InSilicoVA data####
+if(FALSE){
 # isvlimited <- isv[,c(42:52)] #42:52 where injury CoDs are
 # describe(isvlimited)
 # sum(isvlimited) 
@@ -91,14 +84,16 @@ CODdataset <- cbind(isvID,COD)
 # 
 # ####are all IDs from InSilicoVA data in the main data?####
 # sum((isv2.result[,1] %in% umk$idno_original)) #yes, all 953 are in 
-# }
+}
 
+####consistency checks####
 ####if death occurs, "hadva" variable will be filled####
 table(umk$hadva, umk$failure, exclude = NULL)
 sum(umk$hadva, na.rm=T)
-sum(umk$failure, na.rm=T)
-
-test <- umk[umk$hadva==1 & is.na(umk$failure),]
+sum(umk$fail, na.rm=T)
+#can't be checked as hadva has 1 for all episodes of each case 
+#whereas fail occurs at most 1 per person
+#test <- umk[umk$hadva==1 & is.na(umk$failure),]
 
 ####2 FAIL variables####
 table(umk$fail, umk$failure, exclude=NULL)
@@ -107,10 +102,19 @@ table(umk$fail, umk$failure, exclude=NULL)
 table(umk$hivstatus_detail, umk$fail, exclude=NULL)
 
 ####no. of participants####
+#114980
 length(unique(umk$idno_original))
 
+#no. of deaths: 15972
 table(umk$`_d`, umk$fail, exclude=NULL)
-table(umk$`_d`, umk$failure, exclude=NULL)
+#table(umk$`_d`, umk$failure, exclude=NULL) #failure var removed
+
+#no. of deaths which had va
+table(umk$fail, umk$hadva, exclude=NULL)
+#2572+13400=15972
+#2572 didn't have va
+#13400 had va
+
 
 ####checking cases that failed (failure==1)####
 head(umk[umk$failure==1 & !is.na(umk$failure),])
