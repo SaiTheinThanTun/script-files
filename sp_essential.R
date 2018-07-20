45231d2d49137ad1b291556bd4b56f71fad48845
SaiTheinThanTunOnMac <theinthantun.sai@gmail.com>
2018-07-20 23:51:54 +0100

    Wrong replacements

--- sp_essential.R
+++ sp_essential.R
@@ -11,6 +11,7 @@ library(dplyr)
 library(survival)
 library(survminer)
 library(Epi)
+library(popEpi)
 ####function to extract a record####
 reveal <- function(x){
   View(liteumk[liteumk$idno_original==x,])
@@ -38,25 +39,25 @@ CODdataset <- readRDS("CODdataset.RDS")
 length(unique(liteumk$idno_original))
 
 #no. of deaths: 15972
-table(liteumk$`_d`, exclude=NULL)
-#table(liteumk$`_d`, liteumk$failure, exclude=NULL) #failure var removed
+table(liteumk$fail0, exclude=NULL)
+#table(liteumk$fail0, liteumk$failure, exclude=NULL) #failure var removed
 #Because of this resplitting: ‘fail’ variable is now useless as they have been further multiplicated 
 #by the split ie. some may have multiple 1 on the ‘fail’ variable
 
 #no. of deaths which had va (main dataset)
-table(liteumk$`_d`, liteumk$hadva, exclude=NULL)
+table(liteumk$fail0, liteumk$hadva, exclude=NULL)
 #2572+13400=15972
 #2572 didn't have va
 #13400 had va
 
-#tmp <- liteumk[which(is.na(liteumk$`_d`)),]
+#tmp <- liteumk[which(is.na(liteumk$fail0)),]
 
 #datacleaning
-liteumk <- liteumk[which(!is.na(liteumk$`_d`)),] #remove missing from `_d` [already described in report]
+liteumk <- liteumk[which(!is.na(liteumk$fail0)),] #remove missing from fail0 [already described in report]
 #additional cases with same entry and exit dates found out later [on date: 20180719] However, see below
 #liteumk <- liteumk[which(liteumk$entry!=liteumk$exit),] #this is an artefact of spliting by birthday/positive date
-length(unique(liteumk[which(is.na(liteumk$`_d`)),]$idno_original)) #2034 cases only instead of 2037
-#the discrepancy in # is because some cases have records with NA in `_d` (ie. same entry & exit date)
+length(unique(liteumk[which(is.na(liteumk$fail0)),]$idno_original)) #2034 cases only instead of 2037
+#the discrepancy in # is because some cases have records with NA in fail0 (ie. same entry & exit date)
 
 #cases after removal of missing outcomes
 length(unique(liteumk$idno_original))
@@ -67,7 +68,7 @@ if(lfc){
 }
 
 #cases after left censoring
-length(unique(liteumk$idno_original))
+length(unique(liteumk$idno_original)) #88693
 
 ####Right censoring (change terms) for records with age>100####
 if(rtc){
@@ -75,7 +76,7 @@ if(rtc){
 }
 
 #cases after right censoring
-length(unique(liteumk$idno_original))
+length(unique(liteumk$idno_original)) #88686
 
 ####issue with 'residence'####
 if(FALSE){
@@ -104,13 +105,36 @@ dim(dat) #1271372 x37
 sum(!is.na(dat$COD)) #58222 #40185
 
 #no. of deaths
-dim(dat[dat$`_d`==1,]) #7566 deaths
-#sum(c(by(dat$`_d`, dat$idno_original, function(x){sum(x)>=1}))) #same as above
+dim(dat[dat$fail0==1,]) #7566 deaths
+#sum(c(by(dat$fail0, dat$idno_original, function(x){sum(x)>=1}))) #same as above
 
-#sum(c(by(dat$`_d`, dat$idno_original, function(x){sum(x)>1}))) #checking if death in a person is recorded twice, 0
+#sum(c(by(dat$fail0, dat$idno_original, function(x){sum(x)>1}))) #checking if death in a person is recorded twice, 0
 
 #no. of deaths which had VA
-dim(dat[dat$`_d`==1 & !is.na(dat$COD),]) #6222 #4530
+dim(dat[dat$fail0==1 & !is.na(dat$COD),]) #6222 #4530
+
+####Adding new fail variable 'fail2'####
+#loop, test 2 conditions (liteumk$fail==1 & liteumk$COD %in% accidentNames)
+accidentNames <- readRDS('accidentNames.RDS')
+dim(dat) #1271372      37
+if(FALSE){
+  #THIS TAKES TOO LONG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!  
+  
+  for(i in 1:nrow(dat)){
+    if(dat$fail[i]==1 & dat$COD[i] %in% accidentNames) dat$fail2[i] <- 1
+    else dat$fail2[i] <- 0
+  }
+}
+#another faster way of doing the above
+fail2 <- (dat$fail==1)*(dat$COD %in% accidentNames)
+dat <- cbind(dat,fail2)
+dim(dat) #1271372      38
+#sum(is.na(dat$fail2))
+table(dat$fail2, exclude = NULL) #only 817 deaths from accidents
+
+#tmp1 <- dat[dat$fail2==1,]
+#tmp2 <- dat[fail2fast==1,]
+#identical(tmp1, tmp2) #TRUE
 
 #further cleaning done here
 #the aim is to describe only to describe the final (FINAL) dataset
@@ -141,45 +165,76 @@ names(des) <- varDes
 
 #get hivstatus_broad missing to unknown
 dat$hivstatus_broad[is.na(dat$hivstatus_broad)] <- 'Unknown'
+names(dat)[names(dat)=="_t0"] <- "entry"
+names(dat)[names(dat)=="_t"] <- "exit"
+names(dat)[names(dat)==""] <- "fail0"
 
 ####Survival Analysis####
-lx <- Lexis(entry = list(per=cal.yr(entry)), exit = list(per=cal.yr(exit)+.000001), id=idno_original, exit.status=`_d`, data=dat)
+lx <- Lexis(entry = list(per=cal.yr(entry)), exit = list(per=cal.yr(exit)+.000001), id=idno_original, exit.status=fail0, data=dat)
 #saveRDS(lx,'lexisData20180719.RDS')
 #lx <- readRDS('lexisData20180719.RDS')
 #+.000001 was added in exit year in order to overcome artefact of having 
 #same exit and entry date from splitting at first positive dates, and birthdates
 summary(lx)
 summary(lx, by='hivstatus_broad', Rates = T, scale = 1000)
-
 #print(summary(lx, by='hivstatus_broad', Rates = T), digits=4)
 
-du <- cal.yr(dat$exit)-cal.yr(dat$entry)
-sum(du==0) #4486
+View(lx[lx$lex.id==67180,])
+st <- survtab(Surv(time = lex.dur, event = lex.Xst) ~ hivstatus_broad, data = lx, 
+              surv.type = "surv.obs",
+              breaks = list(lex.dur = seq(15, 100, 1)))
 
-samedates <- dat[dat$exit==dat$entry,]
-samedates <- dat[which(du==0),]
-View(samedates)
-length(unique(samedates$idno_original)) #1525
-sum(!is.na(samedates$COD)) #of which 372 has CoD
+#du <- cal.yr(dat$exit)-cal.yr(dat$entry)
+#sum(du==0) #4486
+by(du, dat$hivstatus_broad, sum)
+c(97645.92,79372.85,362437.1)/365
+c(952,2329,4285)/(c(97645.92,79372.85,362437.1)/365)
 
-#plot(survfit(Surv(`_t0`, `_t`, `_d`) ~ 1, data=dat), conf.int=FALSE, mark.time=FALSE) #no comparison
-#sv <- with(dat,Surv(`_t0`, `_t`, `_d`))
-sv <- survfit(Surv(time=`_t0`, time2 = `_t`, event = `_d`) ~ hivstatus_broad, data=dat) #errors
+#records with same entry and exit dates
+#samedates <- dat[dat$exit==dat$entry,]
+#samedates <- dat[which(du==0),]
+#View(samedates)
+#length(unique(samedates$idno_original)) #1525
+#sum(!is.na(samedates$COD)) #of which 372 has CoD
+
+#using only accident CoD
+lx2 <- Lexis(entry = list(per=cal.yr(entry)), exit = list(per=cal.yr(exit)+.000001), id=idno_original, exit.status=fail2, data=dat)
+#saveRDS(lx,'lexisData20180719.RDS')
+#lx <- readRDS('lexisData20180719.RDS')
+#+.000001 was added in exit year in order to overcome artefact of having 
+#same exit and entry date from splitting at first positive dates, and birthdates
+summary(lx2)
+summary(lx2, by='hivstatus_broad', Rates = T, scale = 1000)
+
+sv2 <- survfit(Surv(time=entry, time2 = exit, event = fail2) ~ hivstatus_broad, data=dat) #errors
+summary(sv2)
+summary(sv2, times = c(20, 30, 40))
+plot(sv2, conf.int=FALSE, mark.time=FALSE, col=1:3) #by HIV status
+legend('bottomleft',legend = c('neg','pos','unk'), col=1:3, lty=1)
+
+#the rest# delete after a while
+#plot(survfit(Surv(entry, exit, fail0) ~ 1, data=dat), conf.int=FALSE, mark.time=FALSE) #no comparison
+#sv <- with(dat,Surv(entry, exit, fail0))
+sv <- survfit(Surv(time=entry, time2 = exit, event = fail0) ~ hivstatus_broad, data=dat) #errors
 sv
 summary(sv)
 summary(sv, times = c(20, 30, 40))
 
+sv3 <- survfit(Surv(time=entry, time2 = exit, event = fail0) ~ hivstatus_broad+cluster(idno_original), data=dat) #errors
+sv3
+
 if(FALSE){
   #this method uses each line as a single case, which is not true
-  py <- pyears(Surv(time=`_t0`, time2 = `_t`, event = `_d`) ~ 1, data=dat)
-  py_hiv <- pyears(Surv(time=`_t0`, time2 = `_t`, event = `_d`) ~ hivstatus_broad, data=dat)
+  py <- pyears(Surv(time=entry, time2 = exit, event = fail0) ~ 1, data=dat, scale = 1)
+  py_hiv <- pyears(Surv(time=entry, time2 = exit, event = fail0) ~ hivstatus_broad, data=dat, scale = 1)
   
 }
 
-svcox <- coxph(Surv(time=`_t0`, time2 = `_t`, event = `_d`) ~ hivstatus_broad, data=dat) #errors
-svcoxx <- coxph(Surv(time=`_t0`, time2 = `_t`, event = `_d`) ~ hivstatus_broad+sex, data=dat) #errors
+svcox <- coxph(Surv(time=entry, time2 = exit, event = fail0) ~ hivstatus_broad, data=dat) #errors
+svcoxid <- coxph(Surv(time=entry, time2 = exit, event = fail0) ~ hivstatus_broad+cluster(idno_original), data=dat) #errors
+svcoxx <- coxph(Surv(time=entry, time2 = exit, event = fail0) ~ hivstatus_broad+sex, data=dat) #errors
 ggsurvplot(svcox)
-survdiff(Surv(time=`_t0`, time2 = `_t`, event = `_d`) ~ hivstatus_broad, data=dat)
+survdiff(Surv(time=entry, time2 = exit, event = fail0) ~ hivstatus_broad, data=dat)
 
 plot(sv, conf.int=FALSE, mark.time=FALSE, col=1:3) #by HIV status
 legend('topright',legend = c('neg','pos','unk'), col=1:3, lty=1)
--- testingPackages.R
+++ testingPackages.R
@@ -25,3 +25,249 @@ ggsurvplot(sfit)
 
 
 #another package called Epi and popEpi####
+xcoh <- structure( list( id = c("A", "B", "C"),
+                         birth = c("14/07/1952", "01/04/1954", "10/06/1987"),
+                         entry = c("04/08/1965", "08/09/1972", "23/12/1991"),
+                         exit = c("27/06/1997", "23/05/1995", "24/07/1998"),
+                         fail = c(1, 0, 1) ),
+                   .Names = c("id", "birth", "entry", "exit", "fail"),
+                   row.names = c("1", "2", "3"),
+                   class = "data.frame" )
+
+# Convert the character dates into numerical variables (fractional years)
+xcoh <- cal.yr( xcoh, format="%d/%m/%Y", wh=2:4 )
+# See how it looks
+xcoh
+str( xcoh )
+# Define as Lexis object with timescales calendar time and age
+Lcoh <- Lexis( entry = list( per=entry ),
+               exit = list( per=exit, age=exit-birth ),
+               exit.status = fail,
+               data = xcoh )
+Lcoh
+# Using character states may have undesired effects:
+
+#tinkering
+#another package called Epi and popEpi####
+xcoh <- structure( list( id = c("A", "B", "C", "B"),
+                         birth = c("14/07/1952", "01/04/1954", "10/06/1987", "01/04/1954"),
+                         entry = c("04/08/1965", "08/09/1972", "23/12/1991", "23/05/1995"),
+                         exit = c("27/06/1997", "23/05/1995", "24/07/1998", "23/10/1995"),
+                         fail = c(1, 0, 1, 1) ),
+                   .Names = c("id", "birth", "entry", "exit", "fail"),
+                   row.names = c("1", "2", "3", "4"),
+                   class = "data.frame" )
+
+# Convert the character dates into numerical variables (fractional years)
+xcoh <- cal.yr( xcoh, format="%d/%m/%Y", wh=2:4 )
+# See how it looks
+xcoh
+str( xcoh )
+# Define as Lexis object with timescales calendar time and age
+Lcoh <- Lexis( entry = list( per=entry ),
+               exit = list( per=exit, age=exit-birth ),
+               exit.status = fail,
+               data = xcoh )
+Lcoh
+
+if(FALSE){
+  #viewing the code for Lexis function
+function (entry, exit, duration, entry.status = 0, exit.status = 0, 
+          id, data, merge = TRUE, states, tol = .Machine$double.eps^0.5, 
+          keep.dropped = FALSE) 
+{
+  nmissing <- missing(entry) + missing(exit) + missing(duration)
+  if (nmissing > 2) 
+    stop("At least one of the arguments exit and duration must be supplied")
+  only.exit <- missing(entry.status) && !missing(exit.status)
+  if (!missing(data)) {
+    if (!missing(entry)) {
+      entry <- eval(substitute(entry), data, parent.frame())
+    }
+    if (!missing(exit)) {
+      exit <- eval(substitute(exit), data, parent.frame())
+    }
+    if (!missing(duration)) {
+      duration <- eval(substitute(duration), data, parent.frame())
+    }
+    entry.status <- eval(substitute(entry.status), data, 
+                         parent.frame())
+    exit.status <- eval(substitute(exit.status), data, parent.frame())
+    if (!missing(id)) {
+      id <- eval(substitute(id), data, parent.frame())
+    }
+    if (merge) {
+      data <- as.data.frame(data)
+    }
+  }
+  wh.miss <- any(is.na(entry.status)) + 2 * any(is.na(exit.status))
+  if (wh.miss > 0) 
+    stop("Missing values in ", switch(wh.miss, "entry status", 
+                                      "exit status", "entry AND exit status"))
+  if (only.exit) {
+    if (is.logical(exit.status)) {
+      entry.status <- FALSE
+      cat("NOTE: entry.status has been set to FALSE for all.\n")
+    }
+    if (is.character(exit.status)) {
+      exit.status <- factor(exit.status)
+    }
+    if (is.factor(exit.status)) {
+      entry.status <- factor(rep(levels(exit.status)[1], 
+                                 length(exit.status)), levels = levels(exit.status), 
+                             labels = levels(exit.status))
+      cat("NOTE: entry.status has been set to", paste("\"", 
+                                                      levels(exit.status)[1], "\"", sep = ""), "for all.\n")
+    }
+    if (is.numeric(exit.status)) {
+      entry.status <- rep(0, length(exit.status))
+      cat("NOTE: entry.status has been set to 0 for all.\n")
+    }
+  }
+  if (is.character(entry.status)) 
+    entry.status <- factor(entry.status)
+  if (is.character(exit.status)) 
+    exit.status <- factor(exit.status)
+  if (is.factor(entry.status) || is.factor(exit.status)) {
+    if (is.factor(entry.status) && is.factor(exit.status)) {
+      if (!identical(levels(entry.status), levels(exit.status))) {
+        all.levels = union(levels(entry.status), levels(exit.status))
+        entry.status <- factor(entry.status, levels = all.levels)
+        exit.status <- factor(exit.status, levels = all.levels)
+        cat("Incompatible factor levels in entry.status and exit.status:\n", 
+            "both lex.Cst and lex.Xst now have levels:\n", 
+            all.levels, "\n")
+      }
+    }
+    else {
+      stop("Incompatible classes for entry and exit status")
+    }
+  }
+  else {
+    if (mode(entry.status) != mode(exit.status)) {
+      stop("Incompatible mode for entry and exit status")
+    }
+  }
+  if (nmissing == 2) {
+    if (!missing(exit)) {
+      if (length(exit) > 1) 
+        stop("If 'entry' is omitted, only one timescale can be specified.")
+      else {
+        entry <- exit
+        entry[[1]] <- 0 * entry[[1]]
+        cat("NOTE: entry is assumed to be 0 on the", 
+            names(exit), "timescale.\n")
+      }
+    }
+    else if (!missing(duration)) {
+      if (length(duration) > 1) 
+        stop("If 'entry' is omitted, only one timescale can be specified")
+      else {
+        entry <- duration
+        entry[[1]] <- 0 * entry[[1]]
+        cat("NOTE: entry is assumed to be 0 on the", 
+            names(duration), "timescale.\n")
+      }
+    }
+    else stop("Either exit or duration must be supplied.")
+  }
+  if (!missing(entry)) {
+    entry <- as.data.frame(entry)
+    if (is.null(names(entry))) 
+      stop("entry times have no names")
+    if (any(substr(names(entry), 1, 4) == "lex.")) 
+      stop("names starting with \"lex.\" cannot be used for time scales")
+  }
+  if (!missing(exit)) {
+    exit <- as.data.frame(exit)
+    if (is.null(names(exit))) 
+      stop("exit times have no names")
+    if (any(substr(names(exit), 1, 4) == "lex.")) 
+      stop("names starting with \"lex.\" cannot be used for time scales")
+  }
+  if (!missing(duration)) {
+    duration <- as.data.frame(duration)
+    if (is.null(names(duration))) 
+      stop("duration have no names")
+    if (any(substr(names(duration), 1, 4) == "lex.")) 
+      stop("names starting with \"lex.\" cannot be used for time scales")
+  }
+  if (missing(entry)) {
+    entry <- exit - duration
+  }
+  if (missing(duration)) {
+    full.time.scales <- intersect(names(entry), names(exit))
+    if (length(full.time.scales) == 0) {
+      stop("Cannot calculate duration from entry and exit times")
+    }
+    duration <- exit[, full.time.scales[1]] - entry[, full.time.scales[1]]
+  }
+  if (missing(exit)) {
+    all.time.scales <- names(entry)
+  }
+  else {
+    all.time.scales <- unique(c(names(entry), names(exit)))
+    entry.missing <- setdiff(all.time.scales, names(entry))
+    if (length(entry.missing) > 0) {
+      entry <- cbind(entry, exit[, entry.missing, drop = FALSE] - 
+                       duration)
+    }
+    dura <- exit - entry[, names(exit), drop = FALSE]
+    if (missing(duration)) {
+      duration <- dura[, 1]
+    }
+    ok <- sapply(lapply(dura, all.equal, duration), isTRUE)
+    if (!all(ok)) {
+      stop("Duration is not the same on all time scales")
+    }
+  }
+  if (missing(id)) {
+    id <- 1:nrow(entry)
+  }
+  else if (any(duplicated(id))) {
+  }
+  if (is.data.frame(duration)) 
+    duration <- duration[, 1]
+  lex <- data.frame(entry, lex.dur = duration, lex.Cst = entry.status, 
+                    lex.Xst = exit.status, lex.id = id)
+  if (!missing(states)) {
+    st.lev <- sort(unique(as.character(c(lex$lex.Cst, lex$lex.Xst))))
+    lex$lex.Cst <- factor(as.character(lex$lex.Cst), levels = st.lev, 
+                          labels = states)
+    lex$lex.Xst <- factor(as.character(lex$lex.Xst), levels = st.lev, 
+                          labels = states)
+  }
+  if (!missing(data) && merge) {
+    duplicate.names <- intersect(names(lex), names(data))
+    if (length(duplicate.names) > 0) {
+      stop("Cannot merge data with duplicate names:", paste(duplicate.names, 
+                                                            collapse = " "))
+    }
+    lex <- cbind(lex, data)
+  }
+  short.dur <- lex$lex.dur <= tol
+  if (any(short.dur)) {
+    warning("Dropping ", sum(short.dur), " rows with duration of follow up < tol\n", 
+            if (keep.dropped) 
+              "  The dropped rows are in the attribute 'dropped'\n", 
+            if (keep.dropped) 
+              "  To see them type attr(Obj,'dropped'),\n", 
+            if (keep.dropped) 
+              "  to get rid of them type: attr(Obj,'dropped') <- NULL\n", 
+            if (keep.dropped) 
+              "  - where 'Obj' is the name of your Lexis object")
+    lex <- subset(lex, !short.dur)
+    if (keep.dropped) 
+      attr(lex, "dropped") <- subset(data, short.dur)
+  }
+  attr(lex, "time.scales") <- all.time.scales
+  attr(lex, "time.since") <- rep("", length(all.time.scales))
+  breaks <- vector("list", length(all.time.scales))
+  names(breaks) <- all.time.scales
+  attr(lex, "breaks") <- breaks
+  class(lex) <- c("Lexis", class(lex))
+  return(lex)
+}
+<bytecode: 0x1139925f0>
+  <environment: namespace:Epi>
+  }
