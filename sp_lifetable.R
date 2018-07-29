81911c2771d04f3fbec4366cb49037d25ccb5b54
SaiTheinThanTunOnMac <theinthantun.sai@gmail.com>
2018-07-30 00:43:09 +0100

    Single age women not working

--- sp_lifetable.R
+++ sp_lifetable.R
@@ -5,6 +5,7 @@ library(Epi)
 library(popEpi)
 library(Hmisc)
 source('~/OneDrive/Summer Project/script-files/sp_functions.R')
+creation <- FALSE #WRITE NEW FILES
 
 setwd('~/OneDrive/Summer Project/data')
 dat <- readRDS('dat_lifetable.RDS')
@@ -15,49 +16,44 @@ asmr.all <- pyears(Surv(time=time0, time2 = timex, event = fail0) ~ age, data=da
 #summary(asmr.all, rate = T, ci.r = T)
 #lt(asmr.all, per=10000, ci95=T)
 lt.all <- lt(asmr.all, ageint = 1)
-write.csv(lt.all,paste("~/OneDrive/Summer Project/output/",gsub("\\:","",Sys.time()),"_lt_all.csv",sep = ""))
+if(creation) write.csv(lt.all,paste("~/OneDrive/Summer Project/output/",gsub("\\:","",Sys.time()),"_lt_all.csv",sep = ""))
 
 #ext.inj cause
 asmr.ext <- pyears(Surv(time=time0, time2 = timex, event = fail2) ~ age, data=dat, scale = 1)
 lt.ext <- lt(asmr.ext, ageint = 1)
-write.csv(lt.ext,paste("~/OneDrive/Summer Project/output/",gsub("\\:","",Sys.time()),"_lt_ext.csv",sep = ""))
+if(creation) write.csv(lt.ext,paste("~/OneDrive/Summer Project/output/",gsub("\\:","",Sys.time()),"_lt_ext.csv",sep = ""))
 
 #all cause by 5 year age group
 asmr.all.5 <- pyears(Surv(time=time0, time2 = timex, event = fail0) ~ agegrp, data=dat, scale = 1)
 lt(asmr.all.5, ageint = 5)
 
-asdt <- function(allcause, i_cause, ageint, deletion=TRUE){
-  #Associated single decrement life table
-  #allcause and i_cause are dataframe resulted from `lt` function
-  #ageint defines age interval
-  #deletion specifies if othercause (allcause `minus` i_cause) is used
-  tableNames <- c('Person-years','Event', 'Rate', 'nqx', 'npx', 'lx', 'ndx', 'nLx', 'Tx', 'ex')
-  
-  #check if the dateset input are correct
-  if(sum(colnames(allcause) %in% tableNames)!=length(tableNames)) stop("allcause dataset incorrect")
-  if(sum(colnames(i_cause) %in% tableNames)!=length(tableNames)) stop("i_cause dataset incorrect")
-  
-  #subsetting only necessary sections
-  allcause <- allcause[,which(colnames(allcause %in% tableNames))]
-  i_cause <- i_cause[,which(colnames(i_cause %in% tableNames))]
-  
-  if(deletion){
-    new_nmx <- allcause$rate - i_cause$rate
-  }
-  else {new_nmx <- i_cause$rate}
-  
-  s_npx <- exp(-ageint*new_nmx) #s denotes star or asterisk*
-  s_npx[length(allcause$event)] <- 0
-  
-  s_lx <- NA
-  for(i in 1:length(allcause$event)){
-    if(i==1){s_lx[i] <- 1} #radix of 1 so that ex can be directly got from Tx
-    else {s_lx[i] <- s_lx[i-1]*s_npx[i-1]}
-  }
-  
-  s_ndx <- s_lx*(1-s_npx)
-  
-  s_nLx <- s_ndx/new_nmx
-  
-  
-}
+#ext. injury deleted lifetable####
+#overall
+asdt.inj.del <- asdt(allcause = lt.all, i_cause = lt.ext, ageint = 1)
+if(creation) write.csv(asdt.inj.del,paste("~/OneDrive/Summer Project/output/",gsub("\\:","",Sys.time()),"_asdt_inj_del.csv",sep = ""))
+asdt.inj.del$s_ex[1]-asdt.inj.del$ex[1] #1.84 years
+
+#comparison between HIV groups####
+#Women
+dat.Women <- dat[dat$sex=='Women',]
+#Negative
+asmr.all.Women.Negative <- lt(pyears(Surv(time=time0, time2 = timex, event = fail0) ~ age, data=dat.Women[dat.Women$allFixed=='Negative',], scale = 1), ageint = 1)
+asmr.inj.Women.Negative <- lt(pyears(Surv(time=time0, time2 = timex, event = fail2) ~ age, data=dat.Women[dat.Women$allFixed=='Negative',], scale = 1), ageint = 1)
+asdt.inj.Women.Negative.del <- asdt(allcause = asmr.all.Women.Negative, i_cause = asmr.inj.Women.Negative, ageint = 1)
+Women.Negative <- asdt.inj.Women.Negative.del$s_ex[1]-asdt.inj.Women.Negative.del$ex[1] 
+Women.Negative
+#Positive
+asmr.all.Women.Positive <- lt(pyears(Surv(time=time0, time2 = timex, event = fail0) ~ age, data=dat.Women[dat.Women$allFixed=='Positive',], scale = 1), ageint = 1)
+asmr.inj.Women.Positive <- lt(pyears(Surv(time=time0, time2 = timex, event = fail2) ~ age, data=dat.Women[dat.Women$allFixed=='Positive',], scale = 1), ageint = 1)
+asdt.inj.Women.Positive.del <- asdt(allcause = asmr.all.Women.Positive, i_cause = asmr.inj.Women.Positive, ageint = 1)
+Women.Positive <- asdt.inj.Women.Positive.del$s_ex[1]-asdt.inj.Women.Positive.del$ex[1] 
+Women.Positive
+#Unknown
+asmr.all.Women.Unknown <- lt(pyears(Surv(time=time0, time2 = timex, event = fail0) ~ age, data=dat.Women[dat.Women$allFixed=='Unknown',], scale = 1), ageint = 1)
+asmr.inj.Women.Unknown <- lt(pyears(Surv(time=time0, time2 = timex, event = fail2) ~ age, data=dat.Women[dat.Women$allFixed=='Unknown',], scale = 1), ageint = 1)
+asdt.inj.Women.Unknown.del <- asdt(allcause = asmr.all.Women.Unknown, i_cause = asmr.inj.Women.Unknown, ageint = 1)
+Women.Unknown <- asdt.inj.Women.Unknown.del$s_ex[1]-asdt.inj.Women.Unknown.del$ex[1] 
+Women.Unknown #0.684899
+
+
+
